d61df8efeb5aa0f76f93a988d4360b6ea046dde6
csmartawk-blip <csmartawk@gmail.com>
2025-09-13 06:03:48 +0700

    Update script.js

--- trinidad/script.js
+++ trinidad/script.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentPage = 1;
 
     const results = [
+        { date: '13 SEP 2025', numbers: '4 0 6 2' },
         { date: '12 SEP 2025', numbers: '4 5 0 9' },
         { date: '11 SEP 2025', numbers: '3 8 6 1' },
         { date: '10 SEP 2025', numbers: '7 7 4 9' },
