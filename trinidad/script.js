412685f4978af7809511baa68b12d6a43a1ef7c9
Wgaming168 <csomwk@gmail.com>
2026-03-19 06:23:41 +0700

    Update script.js

--- trinidad/script.js
+++ trinidad/script.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentPage = 1;
 
     const results = [
+        { date: '19 MAR 2026', numbers: '6 4 7 2' },
         { date: '18 MAR 2026', numbers: '6 9 7 3' },
         { date: '17 MAR 2026', numbers: '9 1 0 7' },
         { date: '16 MAR 2026', numbers: '7 0 6 5' },
