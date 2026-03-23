d1e592e72a695479b752e404d39741b11bcfae5d
88romaitaly <88romaitaly@gmail.com>
2026-03-23 21:08:55 +0700

    Create victoria-data.js

---
+++ victoria/victoria-data.js
@@ -0,0 +1,68 @@
+// Victoria Pools 4D Data
+// 4 daily draws: 10 PM, 12 AM, 02 AM, 04 AM GMT+7
+
+var lotteryData = [
+    {
+        date: "2026-03-23",
+        draw10pm: "6140",
+        draw12am: "3014",
+        draw2am: "4361",
+        draw4am: "2154"
+    },
+    {
+        date: "2026-03-22",
+        draw10pm: "7081",
+        draw12am: "6782",
+        draw2am: "7021",
+        draw4am: "8247"
+    },
+    {
+        date: "2026-03-21",
+        draw10pm: "3654",
+        draw12am: "7648",
+        draw2am: "6351",
+        draw4am: "6419"
+    },
+    {
+        date: "2026-03-20",
+        draw10pm: "9261",
+        draw12am: "5337",
+        draw2am: "3285",
+        draw4am: "8402"
+    },
+    {
+        date: "2026-03-19",
+        draw10pm: "1539",
+        draw12am: "1286",
+        draw2am: "9713",
+        draw4am: "9137"
+    },
+    {
+        date: "2026-03-18",
+        draw10pm: "9631",
+        draw12am: "9513",
+        draw2am: "3298",
+        draw4am: "6328"
+    },
+    {
+        date: "2026-03-17",
+        draw10pm: "9564",
+        draw12am: "6354",
+        draw2am: "6325",
+        draw4am: "9654"
+    },
+    {
+        date: "2026-03-16",
+        draw10pm: "8234",
+        draw12am: "2148",
+        draw2am: "2517",
+        draw4am: "1982"
+    },
+    {
+        date: "2026-03-15",
+        draw10pm: "1278",
+        draw12am: "6688",
+        draw2am: "1144",
+        draw4am: "1212"
+    }
+];
