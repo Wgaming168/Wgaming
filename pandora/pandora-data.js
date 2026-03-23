07223e18bc3469113ae25a3d12cfc1cca613cd3f
88romaitaly <88romaitaly@gmail.com>
2026-03-23 20:37:37 +0700

    Create pandora-data.js

---
+++ pandora/pandora-data.js
@@ -0,0 +1,32 @@
+var lotteryData = [
+    {
+        date: "2026-03-23",
+        first: "2638",    // Box 1 - 1st Prize
+        second: "1088",   // Box 2 - 2nd Prize
+        third: "0921"   // Box 3 - 3rd Prize
+    },
+    {
+        date: "2026-03-22",
+        first: "4521",
+        second: "7893",
+        third: "1456"
+    },
+    {
+        date: "2026-03-21",
+        first: "8912",
+        second: "3478",
+        third: "5623"
+    },
+    {
+        date: "2026-03-20",
+        first: "7345",
+        second: "2190",
+        third: "6784"
+    },
+    {
+        date: "2026-03-19",
+        first: "5678",
+        second: "9012",
+        third: "3456"
+    }
+];
