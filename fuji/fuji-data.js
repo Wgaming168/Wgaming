b18c2a11c23f136b077c4077d4e28fffb6f6b3d3
88romaitaly <88romaitaly@gmail.com>
2026-03-23 20:09:29 +0700

    Create fuji-data.js

---
+++ fuji/fuji-data.js
@@ -0,0 +1,62 @@
+var lotteryData = [
+    {
+        date: "2026-03-23",
+        first: "2638",
+        second: "1088",
+        third: "0921"
+    },
+    {
+        date: "2026-03-22",
+        first: "4521",
+        second: "7893",
+        third: "1456"
+    },
+    {
+        date: "2026-03-21",
+        first: "8912",
+        second: "3478",
+        third: "5623"
+    },
+    {
+        date: "2026-03-20",
+        first: "7345",
+        second: "2190",
+        third: "6784"
+    },
+    {
+        date: "2026-03-19",
+        first: "5678",
+        second: "9012",
+        third: "3456"
+    },
+    {
+        date: "2026-03-18",
+        first: "1234",
+        second: "5678",
+        third: "9012"
+    },
+    {
+        date: "2026-03-17",
+        first: "9876",
+        second: "5432",
+        third: "1098"
+    },
+    {
+        date: "2026-03-16",
+        first: "2468",
+        second: "1357",
+        third: "8642"
+    },
+    {
+        date: "2026-03-15",
+        first: "3691",
+        second: "4825",
+        third: "7036"
+    },
+    {
+        date: "2026-03-14",
+        first: "5194",
+        second: "6278",
+        third: "8341"
+    }
+];
