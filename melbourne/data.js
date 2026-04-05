d4c9b61ce6526de7abb9365322a6e950cfa58105
88romaitaly <88romaitaly@gmail.com>
2026-04-05 08:04:01 +0700

    Update data.js

--- melbourne/data.js
+++ melbourne/data.js
@@ -3,6 +3,12 @@
 
 var lotteryData = [
     {
+        date: "2026-04-05",
+        first: "3668",
+        second: "4742",
+        third: "0357"
+    },
+    {
         date: "2026-04-04",
         first: "9573",
         second: "9517",
@@ -61,11 +67,5 @@ var lotteryData = [
         first: "0387",
         second: "2569",
         third: "3354"
-    },
-    {
-        date: "2026-03-25",
-        first: "6739",
-        second: "7487",
-        third: "6786"
     }
 ];
