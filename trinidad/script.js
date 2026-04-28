96bb13fe69a57fe722bf32ad3d622a7322c65f94
88romaitaly <88romaitaly@gmail.com>
2026-04-29 06:08:24 +0700

    Update script.js

--- trinidad/script.js
+++ trinidad/script.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentPage = 1;
 
     const results = [
+        { date: '29 APR 2026', numbers: '0 8 3 1' },
         { date: '28 APR 2026', numbers: '6 4 8 7' },
         { date: '27 APR 2026', numbers: '3 6 1 2' },
         { date: '26 APR 2026', numbers: '5 0 9 4' },
