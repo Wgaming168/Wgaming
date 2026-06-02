e4069f5c9987d8c12e5eecf818f5a312b54c3df5
88romaitaly <88romaitaly@gmail.com>
2026-06-02 21:46:21 +0700

    Update fuji-data.js

--- fuji/fuji-data.js
+++ fuji/fuji-data.js
@@ -1,5 +1,11 @@
 var lotteryData = [
    {
+        date: "2026-06-02",
+        first: "9014",
+        second: "0114",
+        third: "5287"
+    },
+   {
         date: "2026-06-01",
         first: "7627",
         second: "3286",
@@ -40,101 +46,5 @@ var lotteryData = [
         first: "0382",
         second: "3354",
         third: "1597"
-    },
-   {
-        date: "2026-05-25",
-        first: "4790",
-        second: "0914",
-        third: "3541"
-    },
-   {
-        date: "2026-05-24",
-        first: "9793",
-        second: "3271",
-        third: "0278"
-    },
-   {
-        date: "2026-05-23",
-        first: "3587",
-        second: "0628",
-        third: "5973"
-    },
-   {
-        date: "2026-05-22",
-        first: "1521",
-        second: "3327",
-        third: "9647"
-    },
-   {
-        date: "2026-05-21",
-        first: "4540",
-        second: "6479",
-        third: "1354"
-    },
-   {
-        date: "2026-05-20",
-        first: "8034",
-        second: "3452",
-        third: "9164"
-    },
-   {
-        date: "2026-05-19",
-        first: "4178",
-        second: "6247",
-        third: "0143"
-    },
-   {
-        date: "2026-05-18",
-        first: "8096",
-        second: "3891",
-        third: "0647"
-    },
-   {
-        date: "2026-05-17",
-        first: "5068",
-        second: "2579",
-        third: "5461"
-    },
-   {
-        date: "2026-05-16",
-        first: "1376",
-        second: "0096",
-        third: "1672"
-    },
-   {
-        date: "2026-05-15",
-        first: "5301",
-        second: "5087",
-        third: "3596"
-    },
-   {
-        date: "2026-05-14",
-        first: "8906",
-        second: "3698",
-        third: "0451"
-    },
-   {
-        date: "2026-05-13",
-        first: "1804",
-        second: "3614",
-        third: "8064"
-    },
-   {
-        date: "2026-05-12",
-        first: "7149",
-        second: "0324",
-        third: "9406"
-    },
-   {
-        date: "2026-05-11",
-        first: "4762",
-        second: "9046",
-        third: "0047"
-    },
-   {
-        date: "2026-05-10",
-        first: "3904",
-        second: "3258",
-        third: "6219"
     }
 ];
