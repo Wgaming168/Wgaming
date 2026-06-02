6e7b3091d4723b37b854345565282f62dd09436f
88romaitaly <88romaitaly@gmail.com>
2026-06-02 13:18:02 +0700

    Update data.js

--- belfast/data.js
+++ belfast/data.js
@@ -2,6 +2,12 @@
 // Just add new entries at the top with today's date and winning numbers
 var lotteryData = [
     {
+        date: "2026-06-02",
+        first: "4674",
+        second: "6701",
+        third: "9965"
+    },
+    {
         date: "2026-06-01",
         first: "2181",
         second: "5049",
@@ -90,108 +96,6 @@ var lotteryData = [
         first: "5920",
         second: "6709",
         third: "8073"
-    },
-    {
-        date: "2026-05-17",
-        first: "3751",
-        second: "0321",
-        third: "9367"
-    },
-    {
-        date: "2026-05-16",
-        first: "7139",
-        second: "6014",
-        third: "0387"
-    },
-    {
-        date: "2026-05-15",
-        first: "2564",
-        second: "6357",
-        third: "1694"
-    },
-    {
-        date: "2026-05-14",
-        first: "7056",
-        second: "6374",
-        third: "0196"
-    },
-    {
-        date: "2026-05-13",
-        first: "4881",
-        second: "3801",
-        third: "9274"
-    },
-    {
-        date: "2026-05-12",
-        first: "2281",
-        second: "0365",
-        third: "7391"
-    },
-    {
-        date: "2026-05-11",
-        first: "1834",
-        second: "0297",
-        third: "5391"
-    },
-    {
-        date: "2026-05-10",
-        first: "1908",
-        second: "3579",
-        third: "1534"
-    },
-    {
-        date: "2026-05-09",
-        first: "8025",
-        second: "3415",
-        third: "8614"
-    },
-    {
-        date: "2026-05-08",
-        first: "8751",
-        second: "3654",
-        third: "0781"
-    },
-    {
-        date: "2026-05-07",
-        first: "9784",
-        second: "2929",
-        third: "1394"
-    },
-    {
-        date: "2026-05-06",
-        first: "4291",
-        second: "3870",
-        third: "2596"
-    },
-    {
-        date: "2026-05-05",
-        first: "9619",
-        second: "3591",
-        third: "3624"
-    },
-    {
-        date: "2026-05-04",
-        first: "5844",
-        second: "4423",
-        third: "8536"
-    },
-    {
-        date: "2026-05-03",
-        first: "8748",
-        second: "8110",
-        third: "9485"
-    },
-    {
-        date: "2026-05-02",
-        first: "8159",
-        second: "6027",
-        third: "9410"
-    },
-    {
-        date: "2026-05-01",
-        first: "5960",
-        second: "5317",
-        third: "3257"
     }
    
 ];
