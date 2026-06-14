bcc7449ce09ca4be0822cf9d4e2af0f92347cfc8
88romaitaly <88romaitaly@gmail.com>
2026-06-14 14:05:17 +0700

    Update victoria-data.js

--- victoria/victoria-data.js
+++ victoria/victoria-data.js
@@ -6,7 +6,7 @@ var lotteryData = [
     {
         date: "2026-06-14",
         draw10pm: "7215",
-        draw12am: "0000",
+        draw12am: "0667",
         draw2am: "0000",
         draw4am: "0000"
     },
