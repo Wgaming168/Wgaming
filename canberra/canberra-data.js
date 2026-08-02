d6a50c1c51751ace0b4a4aea8b5951782fc4226f
88romaitaly <88romaitaly@gmail.com>
2026-08-02 08:06:31 +0700

    Update canberra-data.js

--- canberra/canberra-data.js
+++ canberra/canberra-data.js
@@ -4,6 +4,12 @@
 var lotteryData = [
     
     {
+        date: "2026-08-02",
+        first: "0321",
+        second: "8790",
+        third: "5541"
+    },
+    {
         date: "2026-08-01",
         first: "7068",
         second: "1503",
