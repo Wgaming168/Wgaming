55251aaa67e69c59b514b58d7431924a78470b1e
88romaitaly <88romaitaly@gmail.com>
2026-08-03 13:00:34 +0700

    Update data.js

--- belfast/data.js
+++ belfast/data.js
@@ -2,14 +2,20 @@
 // Just add new entries at the top with today's date and winning numbers
 var lotteryData = [
     {
+        date: "2026-08-03",
+        first: "8758",
+        second: "0257",
+        third: "9531"
+    },
+    {
         date: "2026-08-02",
-        first: "0298",
+        first: "6478",
         second: "6392",
         third: "0825"
     },
     {
         date: "2026-08-01",
-        first: "4697",
+        first: "2168",
         second: "5280",
         third: "3791"
     },
@@ -36,11 +42,5 @@ var lotteryData = [
         first: "6027",
         second: "5601",
         third: "8732"
-    },
-    {
-        date: "2026-07-27",
-        first: "7480",
-        second: "1852",
-        third: "3051"
     }
 ];
