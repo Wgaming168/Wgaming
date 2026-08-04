fb2abd7595fe93446f4f45d4eebb6ee63a4441e9
88romaitaly <88romaitaly@gmail.com>
2026-08-04 21:26:01 +0700

    Update fuji-data.js

--- fuji/fuji-data.js
+++ fuji/fuji-data.js
@@ -1,5 +1,11 @@
 var lotteryData = [
    {
+        date: "2026-08-04",
+        first: "1853",
+        second: "0257",
+        third: "6509"
+    },
+   {
         date: "2026-08-03",
         first: "0526",
         second: "5882",
@@ -34,11 +40,5 @@ var lotteryData = [
         first: "2413",
         second: "3214",
         third: "9874"
-    },
-   {
-        date: "2026-07-28",
-        first: "5862",
-        second: "8520",
-        third: "9637"
     }
 ];
